36d4f21bf9790d27922ffd9b86bc146f5ea48de0
Lina Cook <lac9209@nyu.edu>
2022-04-26 12:42:56 -0400

    Edited text and formatting.

--- normal_dist/server.R
+++ normal_dist/server.R
@@ -11,20 +11,46 @@ shinyServer(function(input, output) {
     ## Make data for plot, never changing
     dat <- reactive({
         tibble(
-            time = seq(from = 4.5, to = 8.5, by = 0.01),
+            time = seq(from = 4.5, to = 8.5, by = 0.005),
             dist = dnorm(time, mean = 6.5, sd = 0.5)
         )
     })
+    ## Determine side
+    lower_side <- reactive({
+        ifelse(input$test == "Faster", TRUE, FALSE)
+    })
+    ## Calculate p-value
+    p_val <- reactive({
+        round(pnorm(input$point, mean = 6.5, sd = 0.5, 
+                    lower.tail = lower_side()), 3)
+    })
     ## Make plot output
     output$distPlot <- renderPlot({
-        ggplot(data = dat(), aes(time, dist)) +
-            geom_line() +
-            theme_light() +
-            geom_segment(aes(x = input$point, y = 0, xend = input$point, 
-                             yend = dist[which(time == input$point)])) +
-            labs(x = "Time", y = "Probability of Observing")
+        if(lower_side() == TRUE){
+            ggplot(data = dat(), aes(time, dist)) +
+                geom_line() +
+                theme_light() +
+                geom_segment(aes(x = input$point, y = 0, xend = input$point, 
+                                 yend = dist[which(time == input$point)])) +
+                geom_area(data = dplyr::filter(dat(), time <= input$point), 
+                          aes(x = time, y = dist), alpha = 0.2) +
+                labs(x = "Time", y = "Density")
+        }
+        else{
+            ggplot(data = dat(), aes(time, dist)) +
+                geom_line() +
+                theme_light() +
+                geom_segment(aes(x = input$point, y = 0, xend = input$point, 
+                                 yend = dist[which(time == input$point)])) +
+                geom_area(data = dplyr::filter(dat(), time >= input$point), 
+                          aes(x = time, y = dist), alpha = 0.2) +
+                labs(x = "Time", y = "Density")
+        }
     })
     ## Calculate probability
-    output$probability <- renderPrint(round(pnorm(input$point, 
-                                                  mean = 6.5, sd = 0.5), 3))
+    output$probability <- renderUI({
+        strong(paste0("is ", round(pnorm(input$point, mean = 6.5, sd = 0.5, 
+                                lower.tail = lower_side()), 3), "."))
+        })
 })
+
--- normal_dist/ui.R
+++ normal_dist/ui.R
@@ -14,31 +14,37 @@ shinyUI(fluidPage(
 
     # Application title
     titlePanel("Middle School Sprint"),
-    h6("We timed 1000 middle-school-aged boys in a 20-yard sprint and found 
-    that their times fell on a Normal distribution with a mean of 6.5 seconds 
-       and a standard deviation of 0.5 seconds."),
-    h6("Use the slider below to change the point of interest and calculate the 
-       probability that a randomly selected student will be faster than your
-       selected time."),
+    h5("We timed 1000 middle-school boys in a 20-yard sprint and found 
+       that their times fell on a Normal distribution with a mean of 6.5 
+       seconds and a standard deviation of 0.5 seconds. The plot below depicts 
+       this distribution."),
+    h5("The vertical line below the distribution represents a time of interest,
+       or possible observed sprint time. The shaded area represents the 
+       probability that a randomly selected student ran faster than this sprint
+       time. Use the slider below to change the time of interest and 
+       see how the size of the shaded area changes as the time of interest 
+       changes."),
+    h5("We can also observe the probability that a randomly selected student ran
+       slower than the time of interest by changing which side of the time of 
+       interest we shade. We can control this by selecting \"Slower\" below."),
 
     # Sidebar
     sidebarLayout(
         sidebarPanel(
+            radioButtons(inputId = "test", 
+                         label = "The probability that a randomly selected 
+                         student is:", 
+                         choices = c("Faster", "Slower"),
+                         selected = "Faster"),
             sliderInput(inputId = "point",
-                        label = "Point of Interest:",
+                        label = "than this time:",
                         min = 4.5,
                         max = 8.5,
-                        value = 6.5),
-            radioButtons(inputId = "test", 
-                         label = "Test Type", 
-                         choices = c("Less than", "Greater than"),
-                         selected = "Less than")
-        ),
+                        value = 6),
+            uiOutput("probability")),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot"),
-            uiOutput("probability")
+            plotOutput("distPlot")
         )
-    )
-))
+)))
