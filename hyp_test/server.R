aaac43478d072f53d13f80aeec44a49b7cd57f3b
Lina Cook <lac9209@nyu.edu>
2022-05-01 11:23:40 -0400

    Made changes

--- hyp_test/server.R
+++ hyp_test/server.R
@@ -1,46 +1,49 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
-    race_dat <- reactive({
+    game_dat <- reactive({
         dat <- tibble(
-            animals = c(rep("tortoise", times = input$tort_races), 
-                        rep("hare", times = input$hare_races)),
-            time = c(rnorm(input$tort_races, mean = input$tort_speed, sd = 3),
-                     rnorm(input$hare_races, mean = input$hare_speed, sd = 3))
+            results = sample(x = c("You won", "Friend won"), 
+                             size = input$n_rounds, 
+                             replace = TRUE, 
+                             prob = c(input$p_you_win, 1-input$p_you_win))
         )
         
         return(dat)
     })
     
     t_test_output <- reactive({
-        t_test <- t.test(race_dat()$time[which(race_dat()$animals == "tortoise")],
-                         race_dat()$time[which(race_dat()$animals == "hare")],
-                         alternative = "less",
-                         conf.level = input$sig_level)
+        test <- prop.test(x = length(game_dat()$results[which(game_dat()$results == "You won")]), 
+                          n = nrow(game_dat()),
+                          p = 0.5, 
+                          alternative = "greater",
+                          conf.level = input$sig_level)
     })
     
     output$distPlot <- renderPlot({
-        ggplot(race_dat(), aes(x = time, group = animals, fill = animals)) +
-            geom_histogram(alpha = 0.5, position = "identity")
+        ggplot(data = game_dat(), aes(x = results)) +
+            geom_bar(fill = c("skyblue4", "firebrick4")) +
+            scale_x_discrete(limits = c("You won", "Friend won")) +
+            labs(x = NULL, y = "Number of Games Won")
     })
     
-    output$t_stat <- renderPrint(round(t_test_output()$statistic, 2))
     output$p_val <- renderPrint({
-        ifelse(t_test_output()$p.val < 0.05, 
-               paste0(round(t_test_output()$p.val, 2)),
-               paste0(round(t_test_output()$p.val, 2)))})
-    output$estimate <- renderPrint(t_test_output()$estimate)
-    
+        ifelse(t_test_output()$p.val < input$sig_level,
+               paste0("Our test returned a p-value of ",
+                      round(t_test_output()$p.val, 2),
+                      ". We are ", (1 - input$sig_level)*100, 
+                      "% confident that you are on 
+               average better than your friend."),
+               paste0("Our test returned a p-value of ",
+                      round(t_test_output()$p.val, 2),
+                      ". There is not sufficient evidence to say that you are on",
+               " average better than your friend at this significance level."
+               ))
+    })
 })
+    
+    
+    
--- hyp_test/ui.R
+++ hyp_test/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -16,18 +7,9 @@ shinyUI(fluidPage(
     titlePanel("The Tortoise and the Hare"),
     
     # Introduction Text
-    h6("You may be familiar with the famous fable, \"The Tortoise and the Hare.\" 
-       The Hare challenges an unlikely competitor, the Tortoise, to a race to 
-       see which one is \"the fastest.\" While the Hare takes advantage of his 
-       natural strength in speed by acting careless and taking breaks/naps 
-       during the race, the Tortoise continues to move very slowly but without 
-       stopping and finally wins the race. The moral lesson of the story is that 
-       you can be more successful by doing things slowly and steadily than by 
-       acting quickly and carelessly."),
-    h6("As statisticians, we want to take a different spin and investigate if 
-       the Tortoise is faster than the Hare through Hypothesis testing! 
-       Hypothesis testing allows us to set a null hypothesis and test the 
-       validity of this using an alternative hypothesis."),
+    h6("Let's say you and a friend are playing a game. You think you are better
+       than your friend at this game, but you are not sure because you win some
+       rounds and lose other rounds."),
     h6(strong("For this simulation, our null hypothesis is: The Tortoise is not 
               faster than the Hare.")),
     h6(strong("Our alternative hypothesis is: The Tortoise is faster 
@@ -36,20 +18,14 @@ shinyUI(fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            numericInput("tort_races", "Number of Races the Tortoise Ran:",
-                         min = 10, value = 30, max = 100, step = 30),
-            sliderInput("tort_speed",
-                        "Average Tortoise Speed:",
-                        min = 20,
-                        max = 40,
-                        value = 30),
-            numericInput("hare_races", "Number of Races the Hare Ran:",
-                         min = 10, value = 30, max = 100, step = 30),
-            sliderInput("hare_speed",
-                        "Average Hare Speed:",
-                        min = 20,
-                        max = 40,
-                        value = 30),
+            numericInput("n_rounds", 
+                         "Enter the number of rounds to play:",
+                         min = 30, value = 50, max = 100),
+            sliderInput("p_you_win",
+                        "Enter the probability of YOU winning a given round:",
+                        min = 0,
+                        max = 1,
+                        value = 0.5),
             sliderInput("sig_level",
                         "Significance Level:",
                         min = 0.01,
@@ -60,9 +36,7 @@ shinyUI(fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
             plotOutput("distPlot"),
-            uiOutput("t_stat"),
-            uiOutput("p_val"),
-            uiOutput("estimate")
+            uiOutput("p_val")
         )
     )
 ))
